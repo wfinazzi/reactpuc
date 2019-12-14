3ef76370ff780fd3a2b376fb54a8c7b548533b94
Wellington Almeida <wellingtonalmeida@casasandreluiz.org.br>
2019-12-14 14:37:05 -0300

    Primeiro Commit

--- src/index.js
+++ src/index.js
@@ -4,8 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
 
+const helloWorld = 'Hello world'
+const componentBackground = 'blue'
+const props = {
+    className: 'main-title',
+    style: {
+        background: componentBackground,
+        marginLeft: '50px'
+    }
+}
+
+const MyComponent = () => <h1 className="my-title" {... props}>{helloWorld}</h1>
+console.log(MyComponent)
+
+ReactDOM.render(<MyComponent />, rootElement)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
